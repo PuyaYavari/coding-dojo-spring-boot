4f6f79bce0604773044afef060a0400ff39626ca
IllusiveMan <pouyayavari73@gmail.com>
2021-08-07 16:09:27 +0300

    OpenWeather AppId changed in constants

--- src/main/java/com/assignment/spring/Constants.java
+++ src/main/java/com/assignment/spring/Constants.java
@@ -2,7 +2,7 @@ package com.assignment.spring;
 
 public class Constants {
 
-    public static final String APP_ID = "TBD";
+    public static final String APP_ID = "1887b5688db0b032867a66eadb8dcd14";
 
     public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}";
 }
